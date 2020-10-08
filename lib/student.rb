1950366c3a6acc6a99edc1f0737e81dd0aa82e78
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-08 16:51:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
 class Student < User 
+  
+  def initialize(knowledge)
+    @knowledge = [] 
+  end 
+  
+  def learn(teach)
+    @knowledge << teach 
+  end 
+  
+  def knowledge
+    @knowledge
+  end 
 
 end
